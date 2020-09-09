6b1cca700aa59bc3652b34a1c03cce9f03a444c7
caijinglong-b-imac <cjl_spy@163.com>
2020-09-09 09:08:57 +0800

    push with shell

--- dist/index.js
+++ dist/index.js
@@ -1385,30 +1385,12 @@ module.exports = require("os");
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = __importStar(__webpack_require__(186));
+// import * as core from '@actions/core'
 const handle_1 = __webpack_require__(864);
 function exec() {
-    const token = core.getInput('github-token');
+    // const token = core.getInput('github-token')
+    const token = process.env['INPUT_GITHUB_TOKEN'];
     handle_1.run(token);
 }
 exec();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import * as core from '@actions/core'
+// import * as core from '@actions/core'
 import {run} from './handle'
 
 function exec(): void {
-  const token = core.getInput('github-token')
+  // const token = core.getInput('github-token')
+  const token = process.env['INPUT_GITHUB_TOKEN'] as string
   run(token)
 }
 
