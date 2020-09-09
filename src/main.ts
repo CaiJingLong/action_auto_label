7524068abb6e260127c30b6415d813a1e41da08e
caijinglong-b-imac <cjl_spy@163.com>
2020-09-09 10:12:42 +0800

    push with shell

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-// import * as core from '@actions/core'
+import * as core from '@actions/core'
 import {run} from './handle'
 
 function exec(): void {
-  // const token = core.getInput('github-token')
-  const token = process.env['INPUT_GITHUB_TOKEN'] as string
+  const token = core.getInput('github-token')
+  // const token = process.env['INPUT_GITHUB_TOKEN'] as string
   run(token)
 }
 
